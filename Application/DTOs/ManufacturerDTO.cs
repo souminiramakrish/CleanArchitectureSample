d55276f56facc3096b579738946420c711a67350
Soumini Ramakrishnan <souminia@yahoo.co.in>
2026-03-22 18:14:51 +0530

    QuickGrid to show manufacturers with paging and sorting

--- Application/DTOs/ManufacturerDTO.cs
+++ Application/DTOs/ManufacturerDTO.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.DTOs
+﻿namespace Application.DTOs
 {
-    public class ManufacturerDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    public record ManufacturerDTO(int Id, string Name);
+
 }
---
+++ BlazorApp.Shared/Models/PagedResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Models
+{
+    public class PagedResponse<T>
+    {
+        public IReadOnlyList<T> Data { get; init; } = [];
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public int TotalPages { get; init; }
+        public int TotalRecords { get; init; }
+        public bool HasNextPage => PageNumber < TotalPages;
+        public bool HasPreviousPage => PageNumber > 1;
+    }
+}
